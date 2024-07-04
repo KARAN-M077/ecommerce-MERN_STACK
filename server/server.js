75b85ee5f1455b2b798a8a72ea36e131da179384
KARAN-M077 <karanm17ab@gmail.com>
2024-07-04 15:05:38 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -6,9 +6,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
 const port = 3000;
-app.use(cors({
-    origin: 'http://localhost:5173'
-  }))
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.json());
 
